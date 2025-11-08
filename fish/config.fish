ac710c4ffd07430d36e6b2e50f549cc66db14a2f
NicoCaus <nico.caussade@outlook.com>
2025-11-08 12:34:27 -0300

    Initial upload

---
+++ fish/config.fish
@@ -0,0 +1,18 @@
+function fish_greeting
+    fastfetch
+end
+
+function y
+    set tmp (mktemp -t "yazi-cwd.XXXXXX")
+    yazi $argv --cwd-file="$tmp"
+    if read -z cwd <"$tmp"; and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+        builtin cd -- "$cwd"
+    end
+    rm -f -- "$tmp"
+end
+
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+starship init fish | source
---
+++ fish/functions/fishgreeting.fish
@@ -0,0 +1,3 @@
+function fishgreeting
+    fastfetch
+end
